30363bbb615595221d4c0200b9333a5743a61ce2
Kevin Zou <kevin.zp.zou@gmail.com>
2022-02-07 12:31:09 -0800

    Add red colors + gradients

--- src/Colors.js
+++ src/Colors.js
@@ -1,3 +1,9 @@
 export const colors = {
   midnight200: "#E2D6FF",
+  red400: "#FF8989",
+  red500: "#F65C5C",
+  red600: "#F83D3D",
+  gradientMain: "linear-gradient(92.58deg, #0DEFE1 0%, #78FF96 100%)",
+  gradientMainDark: "linear-gradient(90deg, #00DBCE 0%, #00D88A 100%)",
+  gradientMainLight: "linear-gradient(90deg, #D7FFF0 0%, #7BFFCF 100%)",
 };
