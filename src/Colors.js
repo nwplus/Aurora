f7354d7a051e895d3c533a52832a4febcd1d0e1d
Anson Chung <anson1014@hotmail.com>
2022-02-07 19:45:36 -0800

    add emerald color codes

--- src/Colors.js
+++ src/Colors.js
@@ -5,4 +5,9 @@ export const colors = {
   teal400: "#0DEFE1",
   teal500: "#00DBCE",
   teal600: "#00A399",
+  emerald200: "#A6FFDF",
+  emerald300: "#58FFC4",
+  emerald400: "#20FFAF",
+  emerald500: "#00D88A",
+  emerald600: "#00B675"
 };
