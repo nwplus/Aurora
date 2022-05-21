41b15819d1f3ccd2a3557d66f1c47506712ec613
Your Name <new2282307660@gmail.com>
2022-05-20 21:39:24 -0700

    added dummy placeholders

---
+++ src/components/typography/Typography.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const H1 = styled.h1`
+  font-weight: 900;
+  font-size: ${p => p.size || '56px'};
+  line-height: 56px;
+  letter-spacing: 0.4px;
+`
+
+export const H2 = styled.h2`
+`
+
+export const H3 = styled.h3`
+
+`
+export const H4 = styled.h4`
+`
