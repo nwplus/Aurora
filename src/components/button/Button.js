2cf66f1a8b416da84799cd9f1d1296b62d920f2a
DonaldKLee <Donald3010@outlook.com>
2022-06-25 10:26:19 -0700

    Worked on the outline and destruction buttons

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -4,33 +4,59 @@ import styled from 'styled-components';
 // Button
 const Button = (props) => {
 
-        if (props.name === "Outline") {
+      if (props.name === "Outline") {
             var Button = styled.div`
-                font-family: 'HK Grotesk', sans-serif;
-                font-size: 1em;
-                font-weight: 700;
-                background-color: transparent;
-                color: #20FFAF;
-                display: inline-block;
+              font-family: "HK Grotesk", sans-serif;
+              font-size: 1em;
+              font-weight: 700;
+              background-color: transparent;
+              color: #20ffaf;
+              display: inline-block;
+              border-radius: 8px;
+              text-align: center;
+        
+              padding: 0.8rem 2.8rem;
+        
+              transition: 0.3s;
+              cursor: pointer;
+              position: relative;
+              z-index: 0;
+        
+              &:before {
+                content: "";
+                position: absolute;
+                z-index: -1;
+                inset: 0;
+                padding: 3px;
                 border-radius: 8px;
-                text-align: center;
+        
+                background: linear-gradient(to bottom right, #0defe1, #78ff96);
+                -webkit-mask: linear-gradient(#fff 0 0) content-box,
+                  linear-gradient(#fff 0 0);
+                mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
+                -webkit-mask-composite: xor;
+                mask-composite: exclude;
+              }
+                    &:hover {
+                        background: linear-gradient(
+                          90deg,
+                          #d7fff0 0%,
+                          #a5ffde 54.61%,
+                          #7bffcf 100%
+                        );
+                        color: #00a399;
+                        outline: none;
                 
-                padding: 0.8rem 2.8rem;
-             
-                transition: 0.3s;
-                cursor: pointer;
-                outline: 2px solid;
-
-                &:hover {
-                    background: linear-gradient(90deg, #D7FFF0 0%, #A5FFDE 54.61%, #7BFFCF 100%);
-                    color: #00A399;
-                    outline: none;
-                }
-            
-                &:disabled {
-                    color: #8C898F;
-                    outline: 2px solid #8C898F;
-                }`
+                        &:before {
+                          background: none;
+                        }
+                      }
+                
+                      &:disabled {
+                        color: #8c898f;
+                        outline: 2px solid #8c898f;
+                      }
+                    `;
         }
         else if (props.name === "Destructive") {
             var Button = styled.div`
