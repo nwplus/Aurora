344bf7a8f09aa8324b0862d718bc24e824db3e58
samanthatuhseng <samantha.tseng90@gmail.com>
2022-06-24 20:27:55 -0700

    button nav component styles

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,1 +1,57 @@
 import styled from 'styled-components';
+import { colors } from "../colors/Colors";
+
+const ButtonNavigationComponent = styled.button`
+
+    position: absolute;
+    padding: 0;
+    background: none;
+    border: none;
+
+    font-family: 'HK Grotesk';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 48px;
+    /* identical to box height, or 200% */
+
+    display: flex;
+    align-items: center;
+    text-transform: capitalize;
+
+    /* white */
+
+    color: #FFFFFF;
+
+    &:hover {
+        background: linear-gradient(92.58deg, #20FFAF 0%, #78FF96 100%);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        background-clip: text;
+        text-fill-color: transparent;
+    }
+
+    &:disabled {
+        pointer-events: none;
+        color: #8C898F;
+}`;
+
+const Button = (props) => {
+    const { type, text, } = props;
+
+    if (type === 'ButtonNavigationComponent') {
+        return (
+            <ButtonNavigationComponent disabled={props.disabled}>
+                {text}
+            </ButtonNavigationComponent>
+        )
+    }
+    return (
+        <ButtonNavigationComponent disabled={props.disabled}>
+            {text}
+        </ButtonNavigationComponent>
+    )
+}
+
+export default Button;
+
--- src/components/button/Button.stories.jsx
+++ src/components/button/Button.stories.jsx
@@ -1,40 +1,30 @@
 import React from 'react';
+import Button from './Button';
 
-import { Button } from './Button';
-
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
-  component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  title: 'Button',
+
   argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+    type: {
+      options: ['ButtonTextComponent'],
+      control: { type: 'select'},
+    },
+    disabled: {
+      description: 'is component active',
+      options: [true, false],
+      control: {
+        type: 'boolean'
+      }
+    },
+    text: {control: 'text'}
+  }
 };
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'Button',
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
+export const ButtonNavigationComponent = Template.bind({});
+ButtonNavigationComponent.args = {
+  type: 'ButtonNavigationComponent',
+  disabled: true,
+  text: 'Button',
 };
