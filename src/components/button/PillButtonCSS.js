7ed57b23ca9aa4a963eb38bbe48013203045c2eb
Derek Chen <derekc150@gmail.com>
2022-07-12 00:55:14 -0700

    implement pill button

---
+++ src/components/button/PillButtonCSS.js
@@ -0,0 +1,27 @@
+import { colors } from "../colors/index";
+import { PrimaryCSS, SecondaryCSS } from "./ButtonCSS";
+
+export const PillPrimaryCSS = `
+    ${PrimaryCSS}
+    border-radius: 100px;
+`
+
+export const PillSecondaryCSS = `
+    ${SecondaryCSS}
+    border-radius: 100px;
+`
+
+export const PillButtonCSS = `
+    display: table;
+    user-select: none;
+    border-radius: 100px;
+    padding: 0.8rem 1.5rem;
+
+    font-family: "", sans-serif;
+    font-weight: bold;
+
+    transition: all 0.13s linear; 
+    position: relative;
+    overflow: hidden;
+    cursor: pointer;
+`
