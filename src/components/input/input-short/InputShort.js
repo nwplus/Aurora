39b82e90e6f06d287c22d913b7d9eb0de21d4d08
Amy Jo <amyjojjm2001@gmail.com>
2022-08-28 20:05:10 -0700

    InputShortCSS & index file

--- src/components/input/input-short/InputShort.js
+++ src/components/input/input-short/InputShort.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import { errorMessageCSS, InputShortComponentCSS } from "./InputShortCSS";
 import { colors } from "../../colors";
-
 const getDisabledOrErrorStyles = (p) => {
   if (p.disabled) {
     return `border-color: ${colors.grey500}; color:${colors.grey500}; &:hover {
@@ -20,48 +20,17 @@ const getDisabledOrErrorStyles = (p) => {
 };
 
 const InputShortComponent = styled.input`
-  position: relative;
-  transition: 0.3s;
-  background-color: transparent;
-  width: 474px;
-  height: 22px;
-  border-radius: 8px;
-  border: 2px solid white;
-  gap: 10px;
-  color: white;
-  outline: none;
-  z-index: 0;
-
-  padding: 13px;
-  font-family: "HK Grotesk", sans-serif;
-  font-style: normal;
-  font-weight: 400;
-  font-size: 20px;
-  transition: 0.4s;
-
-  &:hover {
-    border-color: ${colors.emerald400};
-  }
-
-  &:focus {
-    color: ${colors.emerald400};
-  }
+  ${InputShortComponentCSS};
   ${(p) => getDisabledOrErrorStyles(p)}
 `;
 
 const ErrorMessage = styled.div`
-  font-size: 18px;
-  color: ${colors.red500};
-  font-family: "HK Grotesk", sans-serif;
-  font-style: normal;
-  font-weight: 400;
-  padding: 4px 0;
-  margin-top: 4px;
+  ${errorMessageCSS};
 `;
 
 const InputShort = ({ disabled, error, errorMessage }) => {
   return (
-    <div>
+    <div style={{ background: "black" }}>
       <InputShortComponent
         id="input-short-component"
         disabled={disabled}
---
+++ src/components/input/input-short/InputShortCSS.js
@@ -0,0 +1,40 @@
+import { colors } from "../../colors";
+
+export const InputShortComponentCSS = `
+position: relative;
+  background-color: transparent;
+  width: 474px;
+  height: 22px;
+  border-radius: 8px;
+  border: 2px solid white;
+  gap: 10px;
+  color: white;
+  outline: none;
+  z-index: 0;
+
+  padding: 13px;
+  font-family: "HK Grotesk", sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  transition: 0.4s;
+
+  &:hover {
+    border-color: ${colors.emerald400};
+  }
+
+  &:focus {
+    color: ${colors.emerald400};
+    border-color:white;
+  }
+`;
+
+export const errorMessageCSS = `
+font-size: 18px;
+color: ${colors.red500};
+font-family: "HK Grotesk", sans-serif;
+font-style: normal;
+font-weight: 400;
+padding: 4px 0;
+margin-top: 4px;
+`;
