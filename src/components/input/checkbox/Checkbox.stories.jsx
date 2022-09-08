e23ccc233adbd9fda09f8f9c43eb6f6ef24a52f3
Amy Jo <amyjojjm2001@gmail.com>
2022-09-07 23:46:48 -0700

    init commit

---
+++ src/components/input/checkbox/Checkbox.stories.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import CheckboxComponent from './CheckboxComponent';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: "Example/Input/Checkbox",
+    component: CheckboxComponent,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+   
+};
+  
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <CheckboxComponent {...args} />;
+
+export const SimpleCheckbox = Template.bind({});
+SimpleCheckbox.args = {
+    label: '',
+    checked: false
+};
---
+++ src/components/input/checkbox/CheckboxComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Checkbox } from '@mui/material';
+const CheckboxComponent = () => {
+  return (
+    <Checkbox defaultChecked/>
+  )
+}
+
+export default CheckboxComponent;
