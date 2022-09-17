1bdb678b1e2585f439cf8f93ecfe4491e328d817
Amy Jo <amyjojjm2001@gmail.com>
2022-09-16 20:08:49 -0700

    disabled checkbox

--- src/components/input/checkbox/Checkbox.stories.jsx
+++ src/components/input/checkbox/Checkbox.stories.jsx
@@ -1,30 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import CheckboxComponent from './CheckboxComponent';
+import CheckboxComponent from "./CheckboxComponent";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-    title: "Example/Input/Checkbox",
-    component: CheckboxComponent,
-    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-   
+  title: "Example/Input/Checkbox",
+  component: CheckboxComponent,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 };
-  
+
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = (args) => <CheckboxComponent {...args} />;
 
 export const SimpleCheckbox = Template.bind({});
 SimpleCheckbox.args = {
-    label: '',
-    checked: false
+  label: "",
+  checked: false,
 };
 
 export const Control = Template.bind({});
 Control.args = {
-    variant: "control",
-    checked: false,
-    active: [true, false],
-    control: {
-        type: 'boolean'
-    }
-}
+  variant: "control",
+  checked: false,
+  disabled: false,
+};
--- src/components/input/checkbox/CheckboxComponent.js
+++ src/components/input/checkbox/CheckboxComponent.js
@@ -1,49 +1,55 @@
-import styled from 'styled-components';
-import React from 'react'
-import { Checkbox } from '@mui/material';
+import React, { useState, useEffect } from "react";
+import { Checkbox } from "@mui/material";
 
 import { colors } from "../../colors/Colors";
 
-const CheckboxComponent = (props) => {
+const CheckboxComponent = ({ disabled, variant }) => {
+  const [checked, setChecked] = useState(false);
 
-    switch(props.variant) {
-      case "control":
-        alert(props.active)
-        return (
-          <Checkbox
-          defaultChecked
+  useEffect(() => {
+    if (disabled) setChecked(false);
+  }, [disabled]);
+
+  switch (variant) {
+    case "control":
+      return (
+        <Checkbox
+          checked={checked}
           sx={{
-            bgcolor: 'transparent',
+            bgcolor: "transparent",
             color: `${colors.white}`,
-            '&.Mui-checked': {
+            "&.Mui-checked": {
               color: `${colors.emerald400}`,
             },
-            '&:hover': {
-              bgcolor: 'transparent',
+            "&:hover": {
+              bgcolor: "transparent",
               color: `${colors.emerald400}`,
-            }
+            },
+          }}
+          disabled={disabled}
+          onClick={() => setChecked(!checked)}
+        />
+      );
+
+    default:
+      // PLACEHOLDER DEFAULT CHECKBOX
+      return (
+        <Checkbox
+          defaultChecked
+          sx={{
+            bgcolor: "transparent",
+            color: `${colors.white}`,
+            "&.Mui-checked": {
+              color: `#000`,
+            },
+            "&:hover": {
+              bgcolor: "transparent",
+              color: `#000`,
+            },
           }}
         />
-        )
-
-        default: // PLACEHOLDER DEFAULT CHECKBOX
-          return (
-            <Checkbox defaultChecked
-            sx={{
-              bgcolor: 'transparent',
-              color: `${colors.white}`,
-              '&.Mui-checked': {
-                color: `#000`,
-              },
-              '&:hover': {
-                bgcolor: 'transparent',
-                color: `#000`,
-              }
-            }}
-          />
-          )
-    }
-
-}
+      );
+  }
+};
 
 export default CheckboxComponent;
