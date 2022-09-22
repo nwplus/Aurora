14ea661cfb98c2de52d743961f88237bb8e57a30
Amy Jo <amyjojjm2001@gmail.com>
2022-09-21 21:52:07 -0700

    weather row 9

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,127 +1,144 @@
 //arrows
-import ArrowUp from "./arrows/arrow-up.svg";
-import ArrowDown from "./arrows/arrow-down.svg";
-import ArrowLeft from "./arrows/arrow-left.svg";
-import ArrowRight from "./arrows/arrow-right.svg";
-import ArrowRightUp from "./arrows/arrow-right-up.svg";
-import ArrowRightDown from "./arrows/arrow-right-down.svg";
-import ArrowLeftDown from "./arrows/arrow-left-down.svg";
-import ArrowLeftUp from "./arrows/arrow-left-up.svg";
-import ChevronUp from "./arrows/chevron-up.svg";
-import ChevronDown from "./arrows/chevron-down.svg";
-import ChevronLeft from "./arrows/chevron-left.svg";
-import ChevronRight from "./arrows/chevron-right.svg";
-import ChevronDoubleUp from "./arrows/chevron-double-up.svg";
-import ChevronDoubleDown from "./arrows/chevron-double-down.svg";
-import ChevronDoubleLeft from "./arrows/chevron-double-left.svg";
-import ChevronDoubleRight from "./arrows/chevron-double-right.svg";
-import CaretDown from "./arrows/caret-down.svg";
-import CaretUp from "./arrows/caret-up.svg";
-import CaretLeft from "./arrows/caret-left.svg";
-import CaretRight from "./arrows/caret-right.svg";
+import ArrowUp from './arrows/arrow-up.svg';
+import ArrowDown from './arrows/arrow-down.svg';
+import ArrowLeft from './arrows/arrow-left.svg';
+import ArrowRight from './arrows/arrow-right.svg';
+import ArrowRightUp from './arrows/arrow-right-up.svg';
+import ArrowRightDown from './arrows/arrow-right-down.svg';
+import ArrowLeftDown from './arrows/arrow-left-down.svg';
+import ArrowLeftUp from './arrows/arrow-left-up.svg';
+import ChevronUp from './arrows/chevron-up.svg';
+import ChevronDown from './arrows/chevron-down.svg';
+import ChevronLeft from './arrows/chevron-left.svg';
+import ChevronRight from './arrows/chevron-right.svg';
+import ChevronDoubleUp from './arrows/chevron-double-up.svg';
+import ChevronDoubleDown from './arrows/chevron-double-down.svg';
+import ChevronDoubleLeft from './arrows/chevron-double-left.svg';
+import ChevronDoubleRight from './arrows/chevron-double-right.svg';
+import CaretDown from './arrows/caret-down.svg';
+import CaretUp from './arrows/caret-up.svg';
+import CaretLeft from './arrows/caret-left.svg';
+import CaretRight from './arrows/caret-right.svg';
 
 // functional
-import Add from "./functional/add.svg";
-import Archive from "./functional/archive.svg";
-import Attachment from "./functional/attachment.svg";
-import Backspace from "./functional/backspace.svg";
-import Ban from "./functional/ban.svg";
-import Bookmark from "./functional/bookmark.svg";
-import Check from "./functional/check.svg";
-import Close from "./functional/close.svg";
-import CloudDownload from "./functional/cloud-download.svg";
-import CloudUpload from "./functional/cloud-upload.svg";
-import Copy from "./functional/copy.svg";
-import DeleteAlt from "./functional/delete-alt.svg";
-import Delete from "./functional/delete.svg";
-import Download from "./functional/download.svg";
-import EditAlt from "./functional/edit-alt.svg";
-import Edit from "./functional/edit.svg";
-import Enter from "./functional/enter.svg";
-import Expand from "./functional/expand.svg";
-import Export from "./functional/export.svg";
-import ExternalLink from "./functional/external-link.svg";
-import EyeOff from "./functional/eye-off.svg";
-import Eye from "./functional/eye.svg";
-import Favorite from "./functional/favorite.svg";
-import FilterAlt from "./functional/filter-alt.svg";
-import Filter from "./functional/filter.svg";
-import Flag from "./functional/flag.svg";
-import Heart from "./functional/heart.svg";
-import LinkAlt from "./functional/link-alt.svg";
-import Link from "./functional/link.svg";
-import Lock from "./functional/lock.svg";
-import Login from "./functional/log-in.svg";
-import Logout from "./functional/log-out.svg";
-import Menu from "./functional/menu.svg";
-import Minimize from "./functional/minimize.svg";
-import OptionsHorizontal from "./functional/options-horizontal.svg";
-import OptionsVertical from "./functional/options-vertical.svg";
-import Percentage from "./functional/percentage.svg";
-import Refresh from "./functional/refresh.svg";
-import Remove from "./functional/remove.svg";
-import ReorderAlt from "./functional/reorder-alt.svg";
-import Reorder from "./functional/reorder.svg";
-import Save from "./functional/save.svg";
-import Search from "./functional/search.svg";
-import Select from "./functional/select.svg";
-import Send from "./functional/send.svg";
-import Settings from "./functional/settings.svg";
-import Share from "./functional/share.svg";
-import Sort from "./functional/sort.svg";
-import Switch from "./functional/switch.svg";
-import Undo from "./functional/undo.svg";
-import Unlock from "./functional/unlock.svg";
-import ZoomIn from "./functional/zoom-in.svg";
-import ZoomOut from "./functional/zoom-out.svg";
+import Add from './functional/add.svg';
+import Archive from './functional/archive.svg';
+import Attachment from './functional/attachment.svg';
+import Backspace from './functional/backspace.svg';
+import Ban from './functional/ban.svg';
+import Bookmark from './functional/bookmark.svg';
+import Check from './functional/check.svg';
+import Close from './functional/close.svg';
+import CloudDownload from './functional/cloud-download.svg';
+import CloudUpload from './functional/cloud-upload.svg';
+import Copy from './functional/copy.svg';
+import DeleteAlt from './functional/delete-alt.svg';
+import Delete from './functional/delete.svg';
+import Download from './functional/download.svg';
+import EditAlt from './functional/edit-alt.svg';
+import Edit from './functional/edit.svg';
+import Enter from './functional/enter.svg';
+import Expand from './functional/expand.svg';
+import Export from './functional/export.svg';
+import ExternalLink from './functional/external-link.svg';
+import EyeOff from './functional/eye-off.svg';
+import Eye from './functional/eye.svg';
+import Favorite from './functional/favorite.svg';
+import FilterAlt from './functional/filter-alt.svg';
+import Filter from './functional/filter.svg';
+import Flag from './functional/flag.svg';
+import Heart from './functional/heart.svg';
+import LinkAlt from './functional/link-alt.svg';
+import Link from './functional/link.svg';
+import Lock from './functional/lock.svg';
+import Login from './functional/log-in.svg';
+import Logout from './functional/log-out.svg';
+import Menu from './functional/menu.svg';
+import Minimize from './functional/minimize.svg';
+import OptionsHorizontal from './functional/options-horizontal.svg';
+import OptionsVertical from './functional/options-vertical.svg';
+import Percentage from './functional/percentage.svg';
+import Refresh from './functional/refresh.svg';
+import Remove from './functional/remove.svg';
+import ReorderAlt from './functional/reorder-alt.svg';
+import Reorder from './functional/reorder.svg';
+import Save from './functional/save.svg';
+import Search from './functional/search.svg';
+import Select from './functional/select.svg';
+import Send from './functional/send.svg';
+import Settings from './functional/settings.svg';
+import Share from './functional/share.svg';
+import Sort from './functional/sort.svg';
+import Switch from './functional/switch.svg';
+import Undo from './functional/undo.svg';
+import Unlock from './functional/unlock.svg';
+import ZoomIn from './functional/zoom-in.svg';
+import ZoomOut from './functional/zoom-out.svg';
 
 //informational
-import CircleAdd from "./informational/circle-add.svg";
-import CircleAddDown from "./informational/circle-arrow-down.svg";
-import CircleAddLeft from "./informational/circle-arrow-left.svg";
-import CircleAddRight from "./informational/circle-arrow-right.svg";
-import CircleAddUp from "./informational/circle-arrow-up.svg";
-import CircleCheck from "./informational/circle-check.svg";
-import CircleError from "./informational/circle-error.svg";
-import CircleHelp from "./informational/circle-help.svg";
-import CircleInformation from "./informational/circle-information.svg";
-import CircleRemove from "./informational/circle-remove.svg";
-import CircleWarning from "./informational/circle-warning.svg";
-import Circle from "./informational/circle.svg";
-import Warning from "./informational/warning.svg";
+import CircleAdd from './informational/circle-add.svg';
+import CircleAddDown from './informational/circle-arrow-down.svg';
+import CircleAddLeft from './informational/circle-arrow-left.svg';
+import CircleAddRight from './informational/circle-arrow-right.svg';
+import CircleAddUp from './informational/circle-arrow-up.svg';
+import CircleCheck from './informational/circle-check.svg';
+import CircleError from './informational/circle-error.svg';
+import CircleHelp from './informational/circle-help.svg';
+import CircleInformation from './informational/circle-information.svg';
+import CircleRemove from './informational/circle-remove.svg';
+import CircleWarning from './informational/circle-warning.svg';
+import Circle from './informational/circle.svg';
+import Warning from './informational/warning.svg';
 
 //communication
-import Call from "./communication/call.svg";
-import Email from "./communication/email.svg";
-import MessageAlt from "./communication/message-alt.svg";
-import Message from "./communication/message.svg";
+import Call from './communication/call.svg';
+import Email from './communication/email.svg';
+import MessageAlt from './communication/message-alt.svg';
+import Message from './communication/message.svg';
 
 //devices
-import Camera from "./devices/camera.svg";
-import Computer from "./devices/computer.svg";
-import Laptop from "./devices/laptop.svg";
-import Mobile from "./devices/mobile.svg";
-import Speakers from "./devices/speakers.svg";
-import Tablet from "./devices/tablet.svg";
+import Camera from './devices/camera.svg';
+import Computer from './devices/computer.svg';
+import Laptop from './devices/laptop.svg';
+import Mobile from './devices/mobile.svg';
+import Speakers from './devices/speakers.svg';
+import Tablet from './devices/tablet.svg';
 
 //views
-import Board from "./views/board.svg";
-import Grid from "./views/grid.svg";
-import Layers from "./views/layers.svg";
-import Layout from "./views/layout.svg";
-import Table from "./views/table.svg";
-import ThreeRows from "./views/three-rows.svg";
-import TwoColumns from "./views/two-columns.svg";
-import TwoRows from "./views/two-rows.svg";
-import Window from "./views/window.svg";
+import Board from './views/board.svg';
+import Grid from './views/grid.svg';
+import Layers from './views/layers.svg';
+import Layout from './views/layout.svg';
+import Table from './views/table.svg';
+import ThreeRows from './views/three-rows.svg';
+import TwoColumns from './views/two-columns.svg';
+import TwoRows from './views/two-rows.svg';
+import Window from './views/window.svg';
+
+//weather
+import Cloud from './weather/cloud.svg';
+import Cloudy from './weather/cloudy.svg';
+import Drop from './weather/drop.svg';
+import Fog from './weather/fog.svg';
+import Moon from './weather/moon.svg';
+import Rain from './weather/rain.svg';
+import Snow from './weather/snow.svg';
+import Snowflake from './weather/snowflake.svg';
+import Storm from './weather/storm.svg';
+import Sun from './weather/sun.svg';
+import SunriseAlt from './weather/sunrise-alt.svg';
+import Sunrise from './weather/sunrise.svg';
+import Sunset from './weather/sunset.svg';
+import Temperature from './weather/temperature.svg';
+import Wind from './weather/wind.svg';
 
 //WYSIWYG
-import Bold from "./WYSIWYG/bold.svg";
-import Italic from "./WYSIWYG/italic.svg";
-import List from "./WYSIWYG/list.svg";
-import Strikethrough from "./WYSIWYG/strikethrough.svg";
-import Text from "./WYSIWYG/text.svg";
-import Underline from "./WYSIWYG/underline.svg";
+import Bold from './WYSIWYG/bold.svg';
+import Italic from './WYSIWYG/italic.svg';
+import List from './WYSIWYG/list.svg';
+import Strikethrough from './WYSIWYG/strikethrough.svg';
+import Text from './WYSIWYG/text.svg';
+import Underline from './WYSIWYG/underline.svg';
 
 export {
   ArrowUp,
@@ -229,6 +246,21 @@ export {
   TwoColumns,
   TwoRows,
   Window,
+  Cloud,
+  Cloudy,
+  Drop,
+  Fog,
+  Moon,
+  Rain,
+  Snow,
+  Snowflake,
+  Storm,
+  Sun,
+  Sunrise,
+  SunriseAlt,
+  Sunset,
+  Temperature,
+  Wind,
   Bold,
   Italic,
   List,
--- src/components/icons/Icons.stories.jsx
+++ src/components/icons/Icons.stories.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { IconsContainer, RowContainer } from "./Icons";
+import React from 'react';
+import { IconsContainer, RowContainer } from './Icons';
 
 import {
   ArrowUp,
@@ -113,145 +113,178 @@ import {
   TwoColumns,
   TwoRows,
   Window,
-} from "../../assets/icons";
+  Cloud,
+  Cloudy,
+  Drop,
+  Fog,
+  Moon,
+  Rain,
+  Snow,
+  Snowflake,
+  Storm,
+  Sun,
+  Sunrise,
+  SunriseAlt,
+  Sunset,
+  Temperature,
+  Wind,
+} from '../../assets/icons';
 
 export default {
-  title: "Icons",
+  title: 'Icons',
 };
 
 const Template = () => (
   <IconsContainer>
     Arrows
     <RowContainer>
-      <img src={ArrowUp} alt="Arrow up" />
-      <img src={ArrowDown} alt="Arrow down" />
-      <img src={ArrowLeft} alt="Arrow left" />
-      <img src={ArrowRight} alt="Arrow right" />
-      <img src={ArrowRightUp} alt="Arrow right up" />
-      <img src={ArrowRightDown} alt="Arrow right down" />
-      <img src={ArrowLeftDown} alt="Arrow left down" />
-      <img src={ArrowLeftUp} alt="Arrow left up" />
-      <img src={ChevronUp} alt="Chevron up" />
-      <img src={ChevronDown} alt="Chevron down" />
-      <img src={ChevronLeft} alt="Chevron left" />
-      <img src={ChevronRight} alt="Chevron right" />
-      <img src={ChevronDoubleUp} alt="Chevron double up" />
-      <img src={ChevronDoubleDown} alt="Chevron double down" />
-      <img src={ChevronDoubleLeft} alt="Chevron double left" />
-      <img src={ChevronDoubleRight} alt="Chevron double right" />
-      <img src={CaretDown} alt="Caret down" />
-      <img src={CaretUp} alt="Caret up" />
-      <img src={CaretLeft} alt="Caret left" />
-      <img src={CaretRight} alt="Caret right" />
+      <img src={ArrowUp} alt='Arrow up' />
+      <img src={ArrowDown} alt='Arrow down' />
+      <img src={ArrowLeft} alt='Arrow left' />
+      <img src={ArrowRight} alt='Arrow right' />
+      <img src={ArrowRightUp} alt='Arrow right up' />
+      <img src={ArrowRightDown} alt='Arrow right down' />
+      <img src={ArrowLeftDown} alt='Arrow left down' />
+      <img src={ArrowLeftUp} alt='Arrow left up' />
+      <img src={ChevronUp} alt='Chevron up' />
+      <img src={ChevronDown} alt='Chevron down' />
+      <img src={ChevronLeft} alt='Chevron left' />
+      <img src={ChevronRight} alt='Chevron right' />
+      <img src={ChevronDoubleUp} alt='Chevron double up' />
+      <img src={ChevronDoubleDown} alt='Chevron double down' />
+      <img src={ChevronDoubleLeft} alt='Chevron double left' />
+      <img src={ChevronDoubleRight} alt='Chevron double right' />
+      <img src={CaretDown} alt='Caret down' />
+      <img src={CaretUp} alt='Caret up' />
+      <img src={CaretLeft} alt='Caret left' />
+      <img src={CaretRight} alt='Caret right' />
     </RowContainer>
     Functional
     <RowContainer>
-      <img src={Add} alt="Add" />
-      <img src={Archive} alt="Archive" />
-      <img src={Attachment} alt="Attachment" />
-      <img src={Backspace} alt="Backspace" />
-      <img src={Ban} alt="Ban" />
-      <img src={Bookmark} alt="Bookmark" />
-      <img src={Check} alt="Check" />
-      <img src={Close} alt="Close" />
-      <img src={CloudDownload} alt="Cloud download" />
-      <img src={CloudUpload} alt="Cloud upload" />
-      <img src={Copy} alt="Copy" />
-      <img src={DeleteAlt} alt="Delete alt" />
-      <img src={Delete} alt="Delete" />
-      <img src={Download} alt="Download" />
-      <img src={EditAlt} alt="Edit alt" />
-      <img src={Edit} alt="Edit" />
-      <img src={Enter} alt="Enter" />
-      <img src={Expand} alt="Expand" />
-      <img src={Export} alt="Export" />
-      <img src={ExternalLink} alt="External link" />
-      <img src={EyeOff} alt="Eye off" />
-      <img src={Eye} alt="Eye" />
-      <img src={Favorite} alt="Favorite" />
-      <img src={FilterAlt} alt="Filter alt" />
-      <img src={Filter} alt="Filter" />
-      <img src={Flag} alt="Flag" />
-      <img src={Heart} alt="Heart" />
-      <img src={LinkAlt} alt="Link alt" />
-      <img src={Link} alt="Link" />
-      <img src={Lock} alt="Lock" />
-      <img src={Login} alt="Login" />
-      <img src={Logout} alt="Logout" />
-      <img src={Menu} alt="Menu" />
-      <img src={Minimize} alt="Minimize" />
-      <img src={OptionsHorizontal} alt="Options horizontal" />
-      <img src={OptionsVertical} alt="Options vertical" />
-      <img src={Percentage} alt="Percentage" />
-      <img src={Refresh} alt="Refresh" />
-      <img src={Remove} alt="Remove" />
-      <img src={ReorderAlt} alt="Reorder alt" />
-      <img src={Reorder} alt="Reorder" />
-      <img src={Save} alt="Save" />
-      <img src={Search} alt="Search" />
-      <img src={Select} alt="Select" />
-      <img src={Send} alt="Sen" />
-      <img src={Settings} alt="Settings" />
-      <img src={Share} alt="Share" />
-      <img src={Sort} alt="Sort" />
-      <img src={Switch} alt="Switch" />
-      <img src={Undo} alt="Undo" />
-      <img src={Unlock} alt="Unlock" />
-      <img src={ZoomIn} alt="Zoom in" />
-      <img src={ZoomOut} alt="Zoom out" />
+      <img src={Add} alt='Add' />
+      <img src={Archive} alt='Archive' />
+      <img src={Attachment} alt='Attachment' />
+      <img src={Backspace} alt='Backspace' />
+      <img src={Ban} alt='Ban' />
+      <img src={Bookmark} alt='Bookmark' />
+      <img src={Check} alt='Check' />
+      <img src={Close} alt='Close' />
+      <img src={CloudDownload} alt='Cloud download' />
+      <img src={CloudUpload} alt='Cloud upload' />
+      <img src={Copy} alt='Copy' />
+      <img src={DeleteAlt} alt='Delete alt' />
+      <img src={Delete} alt='Delete' />
+      <img src={Download} alt='Download' />
+      <img src={EditAlt} alt='Edit alt' />
+      <img src={Edit} alt='Edit' />
+      <img src={Enter} alt='Enter' />
+      <img src={Expand} alt='Expand' />
+      <img src={Export} alt='Export' />
+      <img src={ExternalLink} alt='External link' />
+      <img src={EyeOff} alt='Eye off' />
+      <img src={Eye} alt='Eye' />
+      <img src={Favorite} alt='Favorite' />
+      <img src={FilterAlt} alt='Filter alt' />
+      <img src={Filter} alt='Filter' />
+      <img src={Flag} alt='Flag' />
+      <img src={Heart} alt='Heart' />
+      <img src={LinkAlt} alt='Link alt' />
+      <img src={Link} alt='Link' />
+      <img src={Lock} alt='Lock' />
+      <img src={Login} alt='Login' />
+      <img src={Logout} alt='Logout' />
+      <img src={Menu} alt='Menu' />
+      <img src={Minimize} alt='Minimize' />
+      <img src={OptionsHorizontal} alt='Options horizontal' />
+      <img src={OptionsVertical} alt='Options vertical' />
+      <img src={Percentage} alt='Percentage' />
+      <img src={Refresh} alt='Refresh' />
+      <img src={Remove} alt='Remove' />
+      <img src={ReorderAlt} alt='Reorder alt' />
+      <img src={Reorder} alt='Reorder' />
+      <img src={Save} alt='Save' />
+      <img src={Search} alt='Search' />
+      <img src={Select} alt='Select' />
+      <img src={Send} alt='Sen' />
+      <img src={Settings} alt='Settings' />
+      <img src={Share} alt='Share' />
+      <img src={Sort} alt='Sort' />
+      <img src={Switch} alt='Switch' />
+      <img src={Undo} alt='Undo' />
+      <img src={Unlock} alt='Unlock' />
+      <img src={ZoomIn} alt='Zoom in' />
+      <img src={ZoomOut} alt='Zoom out' />
     </RowContainer>
     Informational
     <RowContainer>
-      <img src={CircleAdd} alt="CircleAdd" />
-      <img src={CircleAddDown} alt="CircleAddDown" />
-      <img src={CircleAddLeft} alt="CircleAddLeft" />
-      <img src={CircleAddRight} alt="CircleAddRight" />
-      <img src={CircleAddUp} alt="CircleAddUp" />
-      <img src={CircleCheck} alt="CircleCheck" />
-      <img src={CircleError} alt="CircleError" />
-      <img src={CircleHelp} alt="CircleHelp" />
-      <img src={CircleInformation} alt="CircleInformation" />
-      <img src={CircleRemove} alt="CircleRemove" />
-      <img src={CircleWarning} alt="CircleWarning" />
-      <img src={Circle} alt="Circle" />
-      <img src={Warning} alt="Warning" />
+      <img src={CircleAdd} alt='CircleAdd' />
+      <img src={CircleAddDown} alt='CircleAddDown' />
+      <img src={CircleAddLeft} alt='CircleAddLeft' />
+      <img src={CircleAddRight} alt='CircleAddRight' />
+      <img src={CircleAddUp} alt='CircleAddUp' />
+      <img src={CircleCheck} alt='CircleCheck' />
+      <img src={CircleError} alt='CircleError' />
+      <img src={CircleHelp} alt='CircleHelp' />
+      <img src={CircleInformation} alt='CircleInformation' />
+      <img src={CircleRemove} alt='CircleRemove' />
+      <img src={CircleWarning} alt='CircleWarning' />
+      <img src={Circle} alt='Circle' />
+      <img src={Warning} alt='Warning' />
     </RowContainer>
     Communication
     <RowContainer>
-      <img src={Call} alt="Call" />
-      <img src={Email} alt="Email" />
-      <img src={MessageAlt} alt="AltMessageAlt" />
-      <img src={Message} alt="Message " />
+      <img src={Call} alt='Call' />
+      <img src={Email} alt='Email' />
+      <img src={MessageAlt} alt='MessageAlt' />
+      <img src={Message} alt='Message ' />
     </RowContainer>
     Devices
     <RowContainer>
-      <img src={Camera} alt="Camera" />
-      <img src={Computer} alt="Computer" />
-      <img src={Laptop} alt="Laptop" />
-      <img src={Mobile} alt="Mobile" />
-      <img src={Speakers} alt="Speakers" />
-      <img src={Tablet} alt="Tablet" />
+      <img src={Camera} alt='Camera' />
+      <img src={Computer} alt='Computer' />
+      <img src={Laptop} alt='Laptop' />
+      <img src={Mobile} alt='Mobile' />
+      <img src={Speakers} alt='Speakers' />
+      <img src={Tablet} alt='Tablet' />
     </RowContainer>
     Views
     <RowContainer>
-      <img src={Board} alt="Board" />
-      <img src={Grid} alt="Grid" />
-      <img src={Layers} alt="Layers" />
-      <img src={Layout} alt="Layout" />
-      <img src={Table} alt="Table" />
-      <img src={ThreeRows} alt="ThreeRows" />
-      <img src={TwoColumns} alt="TwoColumns" />
-      <img src={TwoRows} alt="TwoRows" />
-      <img src={Window} alt="Window" />
+      <img src={Board} alt='Board' />
+      <img src={Grid} alt='Grid' />
+      <img src={Layers} alt='Layers' />
+      <img src={Layout} alt='Layout' />
+      <img src={Table} alt='Table' />
+      <img src={ThreeRows} alt='ThreeRows' />
+      <img src={TwoColumns} alt='TwoColumns' />
+      <img src={TwoRows} alt='TwoRows' />
+      <img src={Window} alt='Window' />
+    </RowContainer>
+    Weather
+    <RowContainer>
+      <img src={Cloud} alt='Cloud' />
+      <img src={Cloudy} alt='Cloudy' />
+      <img src={Drop} alt='Drop' />
+      <img src={Fog} alt='Fog' />
+      <img src={Moon} alt='Moon' />
+      <img src={Rain} alt='Rain' />
+      <img src={Snow} alt='Snow' />
+      <img src={Snowflake} alt='Snowflake' />
+      <img src={Storm} alt='Storm' />
+      <img src={Sun} alt='Sun' />
+      <img src={Sunrise} alt='Sunrise' />
+      <img src={SunriseAlt} alt='SunriseAlt' />
+      <img src={Sunset} alt='Sunset' />
+      <img src={Temperature} alt='Temperature' />
+      <img src={Wind} alt='Wind' />
     </RowContainer>
     WYSIWYG
     <RowContainer>
-      <img src={Bold} alt="Bold" />
-      <img src={Italic} alt="Italic" />
-      <img src={List} alt="List" />
-      <img src={Strikethrough} alt="Strikethrough" />
-      <img src={Text} alt="Text" />
-      <img src={Underline} alt="Underline" />
+      <img src={Bold} alt='Bold' />
+      <img src={Italic} alt='Italic' />
+      <img src={List} alt='List' />
+      <img src={Strikethrough} alt='Strikethrough' />
+      <img src={Text} alt='Text' />
+      <img src={Underline} alt='Underline' />
     </RowContainer>
   </IconsContainer>
 );
