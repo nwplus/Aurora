3919df2c7cb4d3a686ad0abe8d093d23d94c06bb
samanthatuhseng <samantha.tseng90@gmail.com>
2022-10-09 17:06:37 -0700

    fix changes

--- src/components/sidepanel/SidePanel.js
+++ src/components/sidepanel/SidePanel.js
@@ -2,29 +2,24 @@ import { SidePanelCSS } from "./SidePanelCSS";
 import { colors } from "../colors/Colors";
 import styled from "styled-components"
 
-const SidePanel = (props) => {
+const SidePanel = ({ active, label }) => {
+  const inactive = `color: ${colors.grey500}; pointer-events: none`;
+  let styles = "";
 
-  const getActiveStyles = (p) => {
-    const inactive = `color: ${colors.grey500}; pointer-events: none`;
-    let styles = "";
+  if (active) {
+    styles += `${SidePanelCSS}`;
+  } else {
+    styles += inactive;
+  }
 
-    if (p.active) {
-      styles += `${SidePanelCSS}`;
-    } else {
-      styles += inactive;
-    }
-
-    return styles;
-  };
-  
   const SidePanel = styled.div`
 	  ${SidePanelCSS}
-    ${(p)=> getActiveStyles}
+    ${styles}
 `;
 
   return (
-    <SidePanel active = {props.active}>
-      {props.label}
+    <SidePanel active={styles}>
+      {label}
     </SidePanel>
   );
 };
--- src/components/sidepanel/SidePanelCSS.js
+++ src/components/sidepanel/SidePanelCSS.js
@@ -5,6 +5,9 @@ export const SidePanelCSS = `
 width: 139px;
 height: 26px;
 
+display: flex;
+justify-content: center;
+
 font-family: 'HK Grotesk', sans-serif;
 font-style: normal;
 font-weight: 700;
@@ -13,14 +16,13 @@ line-height: 26px;
 
 letter-spacing: -0.45px;
 
-
 color: ${colors.white};
+background: transparent;
+padding: 18px 66px 18px 66px;
 
 &:hover {
     background: ${colors.midnight500};
+    cursor: pointer;
     align-items: center;
-    padding: 18px 66px 18px 66px;
-    position: absolute;
-    gap: 10px;
 }
 `;
