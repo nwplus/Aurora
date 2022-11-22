f2667a0812b493e0ebfd529d6912a857dc36dd97
Amy Jo <amyjojjm2001@gmail.com>
2022-11-21 23:31:11 -0800

    show start dropdown

--- src/components/dropdowns/dropdown-complex-search/DropdownComplexSearch.js
+++ src/components/dropdowns/dropdown-complex-search/DropdownComplexSearch.js
@@ -62,63 +62,61 @@ const DropdownComplexSearchComponent = ({ placeholder, options, error }) => {
   const [selectedOption, setSelectedOption] = useState('');
   const [hasError, setHasError] = useState(error);
 
-  const displayPrompt = event => {
-    if (!selectedOption) {
-      // Render "Start typing to search"
-      return (
-        <OptionsWrapper>
-          <Option>Start typing to search</Option>
-        </OptionsWrapper>
-      )
-    }
+  const [showStart, setShowStart] = useState(false);
 
-    else {
-      // Loop through options to filter schools when typing
-      const filteredSchools = []
-      for (let i=0; i < options.length; i++) {
-        // If found school, append to filteredSchools
-        if (options[i].includes(event.target.value)) {
-          filteredSchools.push(options[i])
+  const displayPrompt = (event) => {
+    if (selectedOption === '') {
+      setShowStart(true);
+    } else {
+      {
+        setShowStart(false);
+        // Loop through options to filter schools when typing
+        const filteredSchools = [];
+        for (let i = 0; i < options.length; i++) {
+          // If found school, append to filteredSchools
+          if (options[i].includes(event.target.value)) {
+            filteredSchools.push(options[i]);
+          }
         }
-      }
 
-      if (filteredSchools.length > 0) {
-        // Render items in filteredSchools as options
-        // alert(filteredSchools)
-        return (
-          <OptionsWrapper>
-            {filteredSchools.map((option, index) => {
-              return (
-                <Option
-                  key={index}
-                  onClick={() => {
-                    setSelectedOption(option);
-                    setShowOptions(false);
-                    setHasError(false);
-                  }}
-                  isLastOption={index === filteredSchools.length - 1}
-                >
-                  {option}
-                </Option>
-              );
-            })}
-          </OptionsWrapper>
-        )
-      }
-      else { 
-        // Render "My school is not listed, use "event.target.value"
-        return (
-          <OptionsWrapper>
-            <Option
-              onClick={() => {
-                setSelectedOption(event.target.value);
-                setShowOptions(false);
-                setHasError(false);
-              }}>
+        if (filteredSchools.length > 0) {
+          // Render items in filteredSchools as options
+          // alert(filteredSchools)
+          return (
+            <OptionsWrapper>
+              {filteredSchools.map((option, index) => {
+                return (
+                  <Option
+                    key={index}
+                    onClick={() => {
+                      setSelectedOption(option);
+                      setShowOptions(false);
+                      setHasError(false);
+                    }}
+                    isLastOption={index === filteredSchools.length - 1}
+                  >
+                    {option}
+                  </Option>
+                );
+              })}
+            </OptionsWrapper>
+          );
+        } else {
+          // Render "My school is not listed, use "event.target.value"
+          return (
+            <OptionsWrapper>
+              <Option
+                onClick={() => {
+                  setSelectedOption(event.target.value);
+                  setShowOptions(false);
+                  setHasError(false);
+                }}
+              >
                 My school is not listed, use {event.target.value}
-            </Option>
-          </OptionsWrapper>
-        )
+              </Option>
+            </OptionsWrapper>
+          );
+        }
       }
     }
   };
@@ -128,7 +126,10 @@ const DropdownComplexSearchComponent = ({ placeholder, options, error }) => {
       <Image src={Search} alt='Search icon' />
 
       <Input
-        onClick={() => setShowOptions(!showOptions)}
+        onClick={(e) => {
+          setShowOptions(!showOptions);
+          displayPrompt(e);
+        }}
         placeholder={placeholder}
         showOptions={showOptions}
         selectedOption={selectedOption}
@@ -138,8 +139,13 @@ const DropdownComplexSearchComponent = ({ placeholder, options, error }) => {
         onChange={(e) => {
           setSelectedOption(e.target.value);
         }}
-        id="Test"
+        id='Test'
       />
+      {showStart ? (
+        <OptionsWrapper>
+          <Option>Start typing to search</Option>
+        </OptionsWrapper>
+      ) : null}
 
       {/* {displayPrompt()} */}
 
