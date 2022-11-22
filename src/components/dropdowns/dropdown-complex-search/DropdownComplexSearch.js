2f1ada67f7dd0cf910a9d9ff9485055b97827e9d
Amy Jo <amyjojjm2001@gmail.com>
2022-11-21 21:25:41 -0800

    filter code

--- src/components/dropdowns/dropdown-complex-search/DropdownComplexSearch.js
+++ src/components/dropdowns/dropdown-complex-search/DropdownComplexSearch.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useState } from 'react';
 import styled from 'styled-components';
-import {  
-          ErrorMsgCSS,
-          OptionCSS,
-          OptionsWrapperCSS,
-          WrapperCSS, 
-          InputCSS } from './DropdownComplexSearchCSS';
+import {
+  ErrorMsgCSS,
+  OptionCSS,
+  OptionsWrapperCSS,
+  WrapperCSS,
+  InputCSS,
+} from './DropdownComplexSearchCSS';
 import { colors } from '../../colors';
 
 import { Search } from '../../../assets/icons';
@@ -22,14 +23,16 @@ const Image = styled.img`
   margin-top: 10px;
 `;
 
-
 const Input = styled.input`
   ${InputCSS}
   padding-left: 40px;
   border-radius: ${({ showOptions }) => (showOptions ? `7px 7px 0 0 ` : `7px`)};
   border: ${({ showOptions }) =>
-    showOptions ? `2px solid ${colors.emerald400}` : `2px solid ${colors.grey500}`};
-  color: ${({ selectedOption }) => selectedOption ? `${colors.emerald400}` : `${colors.grey500}`};
+    showOptions
+      ? `2px solid ${colors.emerald400}`
+      : `2px solid ${colors.grey500}`};
+  color: ${({ selectedOption }) =>
+    selectedOption ? `${colors.emerald400}` : `${colors.grey500}`};
 
   &:hover {
 	color: ${({ error }) => (error ? `white` : ` ${colors.emerald400}`)}
@@ -38,7 +41,6 @@ const Input = styled.input`
 
   border: ${({ error }) => (error ? `2px solid ${colors.red500}` : '')};
 `;
-
 
 const OptionsWrapper = styled.div`
   ${OptionsWrapperCSS}
@@ -57,98 +59,126 @@ const ErrorMsg = styled.div`
 
 const DropdownComplexSearchComponent = ({ placeholder, options, error }) => {
   const [showOptions, setShowOptions] = useState(false);
-  const [selectedOption, setSelectedOption] = useState(null);
+  const [selectedOption, setSelectedOption] = useState('');
   const [hasError, setHasError] = useState(error);
 
-  const displayPrompt = event => {
-    if (event.target.value == 0) {
+  const displayPrompt = () => {
+    if (!selectedOption) {
       // Render "Start typing to search"
-    }
-
-    else {
-      // Loop through options to filter schools when typing
-      const filteredSchools = []
-      for (let i=0; i < options.length; i++) {
-        // If found school, append to filteredSchools
-        if (options[i].includes(event.target.value)) {
-          filteredSchools.push(options[i])
+    } else {
+      let filteredSchools = [];
+      for (let i = 0; i < options.length; i++) {
+        if (options[i].toLowerCase().includes(selectedOption.toLowerCase())) {
+          filteredSchools.push(options[i]);
         }
       }
 
-      if (filteredSchools.length > 0) {
-        // Render items in filteredSchools as options
-        // alert(filteredSchools)
-        return (
-          <OptionsWrapper>
-            {filteredSchools.map((option, index) => {
-              return (
-                <Option
-                  key={index}
-                  onClick={() => {
-                    setSelectedOption(option);
-                    setShowOptions(false);
-                    setHasError(false);
-                  }}
-                  isLastOption={index === filteredSchools.length - 1}
-                >
-                  {option}
-                </Option>
-              );
-            })}
-          </OptionsWrapper>
-        )
-      }
-      else { 
-        // Render "My school is not listed, use "event.target.value"
-        //alert("My school is not listed, use " + event.target.value)
-      }
+      return (
+        <OptionsWrapper>
+          {filteredSchools.map((option, index) => {
+            return (
+              <Option
+                key={index}
+                onClick={() => {
+                  setSelectedOption(option);
+                  setShowOptions(false);
+                  setHasError(false);
+                }}
+                isLastOption={index === filteredSchools.length - 1}
+              >
+                {option}
+              </Option>
+            );
+          })}
+        </OptionsWrapper>
+      );
     }
+  };
 
-  }
+  // const displayPrompt = () => {
+  //   if (selectedOption && selectedOption.length == 0) {
+  //     // Render "Start typing to search"
+  //   } else {
+  //     // Loop through options to filter schools when typing
+  //     const filteredSchools = [];
+  //     for (let i = 0; i < options.length; i++) {
+  //       // If found school, append to filteredSchools
+  //       if (options[i].includes(selectedOption)) {
+  //         filteredSchools.push(options[i]);
+  //       }
+  //     }
+
+  //     if (filteredSchools.length > 0) {
+  //       // Render items in filteredSchools as options
+  //       // alert(filteredSchools)
+  //       return (
+  //         <OptionsWrapper>
+  //           {filteredSchools.map((option, index) => {
+  //             return (
+  //               <Option
+  //                 key={index}
+  //                 onClick={() => {
+  //                   setSelectedOption(option);
+  //                   setShowOptions(false);
+  //                   setHasError(false);
+  //                 }}
+  //                 isLastOption={index === filteredSchools.length - 1}
+  //               >
+  //                 {option}
+  //               </Option>
+  //             );
+  //           })}
+  //         </OptionsWrapper>
+  //       );
+  //     } else {
+  //       // Render "My school is not listed, use "event.target.value"
+  //       //alert("My school is not listed, use " + event.target.value)
+  //     }
+  //   }
+  // };
 
   return (
     <Wrapper>
-        
-        <Image
-          src={ Search }
-          alt='Search icon'
-        />
+      <Image src={Search} alt='Search icon' />
 
-        <Input 
-            onClick={() => setShowOptions(!showOptions)}
-            placeholder={placeholder}
-            showOptions={showOptions}
-            selectedOption={selectedOption}
-            onInput={displayPrompt}
-            value={selectedOption}
-            error={hasError}
-        />
+      <Input
+        onClick={() => setShowOptions(!showOptions)}
+        placeholder={placeholder}
+        showOptions={showOptions}
+        selectedOption={selectedOption}
+        onInput={displayPrompt}
+        value={selectedOption}
+        error={hasError}
+        onChange={(e) => {
+          setSelectedOption(e.target.value);
+        }}
+      />
 
-        {/* {displayPrompt} */}
+      {displayPrompt()}
 
-        {/* {showOptions && options.length > 0 && (
-          <OptionsWrapper>
-            {options.map((option, index) => {
-              return (
-                <Option
-                  key={index}
-                  onClick={() => {
-                    setSelectedOption(option);
-                    setShowOptions(false);
-                    setHasError(false);
-                  }}
-                  isLastOption={index === options.length - 1}
-                >
-                  {option}
-                </Option>
-              );
-            })}
+      {/* {showOptions && options.length > 0 && (
+        <OptionsWrapper>
+          {options.map((option, index) => {
+            return (
+              <Option
+                key={index}
+                onClick={() => {
+                  setSelectedOption(option);
+                  setShowOptions(false);
+                  setHasError(false);
+                }}
+                isLastOption={index === options.length - 1}
+              >
+                {option}
+              </Option>
+            );
+          })}
+        </OptionsWrapper>
+      )} */}
 
-
-          </OptionsWrapper>
-        )} */}
-        
-        {!showOptions && hasError && <ErrorMsg>Please enter your school</ErrorMsg>}
+      {!showOptions && hasError && (
+        <ErrorMsg>Please enter your school</ErrorMsg>
+      )}
     </Wrapper>
   );
 };
