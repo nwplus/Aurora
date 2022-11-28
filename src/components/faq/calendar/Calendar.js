d2e1c45a7b067858e4857bacb2442f4888ea1705
Amy Jo <amyjojjm2001@gmail.com>
2022-11-27 17:28:26 -0800

    calendar format

--- src/components/faq/calendar/Calendar.js
+++ src/components/faq/calendar/Calendar.js
@@ -1,74 +1,74 @@
 import React from 'react';
-import styled from "styled-components";
-import { CalendarActiveCSS, CalendarInactiveCSS, CalendarTitleCSS, CalendarTagCSS} from "./CalendarCSS"
+import styled from 'styled-components';
+import {
+  CalendarActiveCSS,
+  CalendarInactiveCSS,
+  CalendarTitleCSS,
+  CalendarTagCSS,
+} from './CalendarCSS';
 
 import CalendarTagImage from '../../../assets/tag_filled.svg';
 
-const CalendarTitle = styled.p `
-    ${CalendarTitleCSS}
-`
+const CalendarTitle = styled.p`
+  ${CalendarTitleCSS}
+`;
 
 // Calendar
-const CalendarComponent = ({description, active, title, date}) => {
-    let styles = ""
-    // If description is too long, cut it
-    const checkDescription = () => {
-        let modified_calendar_description = description
-        if (description.length > 64) {
-            modified_calendar_description = description.substring(0, 64) + " ..."
-        }
-    
-        return modified_calendar_description;
-    }
-    const calendarStyles = () => {
-        // true
-        if (active) {
-            // Active calendar
-            return {CalendarActiveCSS};
-            
-        }
-        // false
-        else {
-            // Inactive calendar
-            return {CalendarInactiveCSS};
-            
-        }
+const CalendarComponent = ({ description, active, title, date }) => {
+  // If description is too long, cut it
+  const checkDescription = () => {
+    let modified_calendar_description = description;
+    if (description.length > 64) {
+      modified_calendar_description = description.substring(0, 64) + ' ...';
     }
 
-    const CalendarComponent = styled.div `
-        ${calendarStyles}
-    `
-
-    // Tag image should not be visible for inactive calendars
-    const TagVisibility = () => {
-        let tagstyles = ""
-        // false
-        if (!active) {
-            // Active calendar
-            tagstyles += `
-                display:none;
-            `
-        }
-        return tagstyles;
+    return modified_calendar_description;
+  };
+  const calendarStyles = () => {
+    // true
+    if (active) {
+      // Active calendar
+      return { CalendarActiveCSS };
     }
- 
-    const CalendarTag = styled.div`
-        ${CalendarTagCSS}
-        ${TagVisibility()}
-    `
-    return (
-        // CalendarDes will disappear if the text inside it is empty
-        <CalendarComponent>
-            <CalendarTitle>
-                {title}
-                <CalendarTag>
-                    <img src={CalendarTagImage}/>
-                </CalendarTag>
-            </CalendarTitle>
-            <p>{date}</p>
-            <p>{checkDescription()}</p>
-        </CalendarComponent>
-    )
-}
+    // false
+    else {
+      // Inactive calendar
+      return { CalendarInactiveCSS };
+    }
+  };
+
+  const CalendarComponent = styled.div`
+    ${calendarStyles}
+  `;
+
+  // Tag image should not be visible for inactive calendars
+  const TagVisibility = () => {
+    let tagstyles = '';
+    // false
+    if (!active) {
+      // Active calendar
+      tagstyles += `display:none;`;
+    }
+    return tagstyles;
+  };
+
+  const CalendarTag = styled.div`
+    ${CalendarTagCSS}
+    ${TagVisibility()}
+  `;
+  return (
+    // CalendarDes will disappear if the text inside it is empty
+    <CalendarComponent>
+      <CalendarTitle>
+        {title}
+        <CalendarTag>
+          <img src={CalendarTagImage} />
+        </CalendarTag>
+      </CalendarTitle>
+      <p>{date}</p>
+      <p>{checkDescription()}</p>
+    </CalendarComponent>
+  );
+};
 
 export { CalendarComponent };
