9283be3c075ff74e72f7f3550605a76b3a64ca3c
Amy Jo <amyjojjm2001@gmail.com>
2022-11-27 17:29:53 -0800

    format timer

--- src/components/indicator/timer/Timer.js
+++ src/components/indicator/timer/Timer.js
@@ -1,100 +1,100 @@
-import styled from "styled-components";
-import React, { useState, useEffect, useCallback } from "react";
+import styled from 'styled-components';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import {
   TimerCSS,
   TimerContainerCSS,
   TimerHeadingCSS,
   TimerNumsCSS,
-  TimerLabelsCSS
-} from "./TimerCSS";
+  TimerLabelsCSS,
+} from './TimerCSS';
 
 const TimerComponent = styled.div`
-    ${TimerCSS}
+  ${TimerCSS}
 `;
 
 const TimerContainer = styled.div`
-    ${TimerContainerCSS}
+  ${TimerContainerCSS}
 `;
 
 const TimerHeading = styled.h1`
-    ${TimerHeadingCSS}
+  ${TimerHeadingCSS}
 `;
 
 const Timer = styled.span`
-    ${TimerNumsCSS}
+  ${TimerNumsCSS}
 `;
 
 const TimerLabels = styled.div`
-    ${TimerLabelsCSS}
+  ${TimerLabelsCSS}
 `;
 
-const TimerComponentContainer = ({isActive, time}) => {
-    const [timeRemaining, setTimeRemaining] = useState(time);
-    
-    const getTime = useCallback(() => {
-        let newTime = timeRemaining - 1;
+const TimerComponentContainer = ({ isActive, time }) => {
+  const [timeRemaining, setTimeRemaining] = useState(time);
 
-        setTimeRemaining(newTime);
-    }, [timeRemaining]);
+  const getTime = useCallback(() => {
+    let newTime = timeRemaining - 1;
 
-    useEffect(() => {
-        if (isActive && timeRemaining > 0) {
-            const interval = setInterval(() => getTime(), 1000);
-    
-            return () => clearInterval(interval);
-        }
-    }, [getTime, timeRemaining, isActive]);
+    setTimeRemaining(newTime);
+  }, [timeRemaining]);
 
-    const formatTime = () => {
-        let seconds = Math.floor(timeRemaining % 60);
-        let minutes = Math.floor(timeRemaining / 60) % 60;
-        let hours = Math.floor(timeRemaining / 3600) % 24;
-        let days = Math.floor(timeRemaining / 86400);
+  useEffect(() => {
+    if (isActive && timeRemaining > 0) {
+      const interval = setInterval(() => getTime(), 1000);
 
-        let res = "";
-        
-        if (days < 10) {
-            res += "0" + days + ":";
-        } else {
-            res += days + ":"; 
-        }
-        
-        if (hours < 10) {
-            res += "0" + hours + ":";
-        } else {
-            res += hours + ":"; 
-        }
-        
-        if (minutes < 10) {
-            res += "0" + minutes + ":";
-        } else {
-            res += minutes + ":"; 
-        }
+      return () => clearInterval(interval);
+    }
+  }, [getTime, timeRemaining, isActive]);
 
-        if (seconds < 10) {
-            res += "0" + seconds;
-        } else {
-            res += seconds; 
-        }
+  const formatTime = () => {
+    let seconds = Math.floor(timeRemaining % 60);
+    let minutes = Math.floor(timeRemaining / 60) % 60;
+    let hours = Math.floor(timeRemaining / 3600) % 24;
+    let days = Math.floor(timeRemaining / 86400);
 
-        return res; 
+    let res = '';
+
+    if (days < 10) {
+      res += '0' + days + ':';
+    } else {
+      res += days + ':';
     }
 
-    return (
-        <TimerComponent>
-            <TimerHeading>HACKING ENDS IN...</TimerHeading>
-            <TimerContainer>
-                <Timer>{formatTime()}</Timer>
-                <TimerLabels>
-                    <p>day</p>
-                    <p>hr</p>
-                    <p>min</p>
-                    <p>sec</p>
-                </TimerLabels>
-            </TimerContainer>
-        </TimerComponent>
-    );
+    if (hours < 10) {
+      res += '0' + hours + ':';
+    } else {
+      res += hours + ':';
+    }
+
+    if (minutes < 10) {
+      res += '0' + minutes + ':';
+    } else {
+      res += minutes + ':';
+    }
+
+    if (seconds < 10) {
+      res += '0' + seconds;
+    } else {
+      res += seconds;
+    }
+
+    return res;
+  };
+
+  return (
+    <TimerComponent>
+      <TimerHeading>HACKING ENDS IN...</TimerHeading>
+      <TimerContainer>
+        <Timer>{formatTime()}</Timer>
+        <TimerLabels>
+          <p>day</p>
+          <p>hr</p>
+          <p>min</p>
+          <p>sec</p>
+        </TimerLabels>
+      </TimerContainer>
+    </TimerComponent>
+  );
 };
 
 export { TimerComponentContainer };
