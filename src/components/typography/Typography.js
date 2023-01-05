7980ca29a4393fd9182dd33ddd2353021d9b3e17
Amy Jo <amyjojjm2001@gmail.com>
2023-01-05 02:11:02 -0800

    fix typography for package

--- src/components/typography/Typography.js
+++ src/components/typography/Typography.js
@@ -1,28 +1,32 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const H1 = styled.h1`
   font-weight: 900;
-  font-size: ${(p) => p.size || '3.5rem'};
+  font-size: ${(p) => p.size || "3.5rem"};
   line-height: 3.5rem;
   letter-spacing: 0.4px;
+  display: block;
 `;
 
 export const H2 = styled.h2`
   font-weight: 800;
-  font-size: ${(p) => p.size || '2.625rem'};
+  font-size: ${(p) => p.size || "2.625rem"};
   line-height: 2.625rem;
   letter-spacing: 0.38px;
+  display: block;
 `;
 
 export const H3 = styled.h3`
   font-weight: 600;
-  font-size: ${(p) => p.size || '2rem'};
+  font-size: ${(p) => p.size || "2rem"};
   line-height: 2rem;
+  display: block;
 `;
 export const H4 = styled.h4`
   font-weight: 700;
-  font-size: ${(p) => p.size || '24px'};
+  font-size: ${(p) => p.size || "24px"};
   line-height: 26px;
+  display: block;
 `;
 
 export const Paragraph = styled.p`
@@ -31,14 +35,15 @@ export const Paragraph = styled.p`
   font-size: 14px;
   line-height: 21px;
   margin: 0;
+  display: block;
 `;
 
 export const HeadingCaption = styled.h6`
   font-weight: 700;
-  font-size: ${(p) => p.size || '0.94rem'};
+  font-size: ${(p) => p.size || "0.94rem"};
   line-height: 0.94rem;
   word-spacing: 8px;
-  margin-top: -20px;
+  display: block;
 `;
 
 export const BodyTitle = styled.h5`
@@ -46,6 +51,7 @@ export const BodyTitle = styled.h5`
   font-weight: 800;
   font-size: 18px;
   line-height: 22px;
+  display: block;
 `;
 
 export const BodyCaption = styled.h5`
@@ -54,13 +60,15 @@ export const BodyCaption = styled.h5`
   font-size: 15px;
   line-height: 20px;
   word-spacing: 8px;
-  margin-top: -20px;
+  display: block;
 `;
 
 export const HeadingMain = styled.h1`
   font-weight: 600;
-  font-size: ${(p) => p.size || '15rem'};
+  font-size: ${(p) => p.size || "15rem"};
   line-height: 15rem;
+  display: block;
+  margin: 0;
 `;
 
 export const BoldFontCaption = styled.h4`
@@ -68,7 +76,7 @@ export const BoldFontCaption = styled.h4`
   font-weight: 900;
   font-size: 34px;
   line-height: 41px;
-  margin-top: -25px;
+  display: block;
 `;
 
 export const H1Caption = styled.h4`
@@ -76,11 +84,12 @@ export const H1Caption = styled.h4`
   font-weight: 400;
   font-size: 17px;
   line-height: 22px;
-  margin: 20px auto 10px auto;
+  display: block;
 `;
 
 export const FontCaption = styled.h5`
   font-weight: 400;
-  font-size: ${(p) => p.size || '1.5rem'};
+  font-size: ${(p) => p.size || "1.5rem"};
   line-height: 1.5rem;
+  display: block;
 `;
