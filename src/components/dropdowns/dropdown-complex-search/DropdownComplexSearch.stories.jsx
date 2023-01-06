a4ab2c5db524f4586564ed5f6b825cea384f01c2
Amy Jo <amyjojjm2001@gmail.com>
2023-01-05 22:34:04 -0800

    fix storybook imports & eslint

--- src/components/dropdowns/dropdown-complex-search/DropdownComplexSearch.stories.jsx
+++ src/components/dropdowns/dropdown-complex-search/DropdownComplexSearch.stories.jsx
@@ -1,26 +1,25 @@
-import DropdownComplexSearchComponent from "./DropdownComplexSearch";
-
+import { DropdownComplexSearchComponent } from "./DropdownComplexSearch";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-    title: 'Example/Dropdowns/DropdownComplexSearch',
-    component: DropdownComplexSearchComponent,
-  };
-  
-  const Template = (args) => <DropdownComplexSearchComponent {...args} />;
-  
-  const sampleOptions = [
-    'University of British Columbia, Vancouver',
-    'University of California Berkley',
-    'University of California Los Angeles',
-    'University of New South Wales, Sydney',
-    'University of Washington'
-  ];
-  
-  export const PrimarySelect = Template.bind({});
-  // More on args: https://storybook.js.org/docs/react/writing-stories/args
-  PrimarySelect.args = {
-    placeholder: 'Enter your school',
-    options: sampleOptions,
-    error: false,
-  };  
+  title: "Example/Dropdowns/DropdownComplexSearch",
+  component: DropdownComplexSearchComponent,
+};
+
+const Template = (args) => <DropdownComplexSearchComponent {...args} />;
+
+const sampleOptions = [
+  "University of British Columbia, Vancouver",
+  "University of California Berkley",
+  "University of California Los Angeles",
+  "University of New South Wales, Sydney",
+  "University of Washington",
+];
+
+export const PrimarySelect = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+PrimarySelect.args = {
+  placeholder: "Enter your school",
+  options: sampleOptions,
+  error: false,
+};
--- src/components/icons/Icons.stories.jsx
+++ src/components/icons/Icons.stories.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { IconsContainer, RowContainer } from './Icons';
+import React from "react";
+import { IconsContainer, RowContainer } from "./Icons";
 
 import {
   ArrowUp,
@@ -180,224 +180,224 @@ import {
   ShoppingCart,
   Tag,
   Webcam,
-} from '../../assets/icons';
+} from "../../assets/icons";
 
 export default {
-  title: 'Icons',
+  title: "Icons",
 };
 
 const Template = () => (
   <IconsContainer>
     Arrows
     <RowContainer>
-      <img src={ArrowUp} alt='Arrow up' />
-      <img src={ArrowDown} alt='Arrow down' />
-      <img src={ArrowLeft} alt='Arrow left' />
-      <img src={ArrowRight} alt='Arrow right' />
-      <img src={ArrowRightUp} alt='Arrow right up' />
-      <img src={ArrowRightDown} alt='Arrow right down' />
-      <img src={ArrowLeftDown} alt='Arrow left down' />
-      <img src={ArrowLeftUp} alt='Arrow left up' />
-      <img src={ChevronUp} alt='Chevron up' />
-      <img src={ChevronDown} alt='Chevron down' />
-      <img src={ChevronLeft} alt='Chevron left' />
-      <img src={ChevronRight} alt='Chevron right' />
-      <img src={ChevronDoubleUp} alt='Chevron double up' />
-      <img src={ChevronDoubleDown} alt='Chevron double down' />
-      <img src={ChevronDoubleLeft} alt='Chevron double left' />
-      <img src={ChevronDoubleRight} alt='Chevron double right' />
-      <img src={CaretDown} alt='Caret down' />
-      <img src={CaretUp} alt='Caret up' />
-      <img src={CaretLeft} alt='Caret left' />
-      <img src={CaretRight} alt='Caret right' />
+      <img src={ArrowUp} alt="Arrow up" />
+      <img src={ArrowDown} alt="Arrow down" />
+      <img src={ArrowLeft} alt="Arrow left" />
+      <img src={ArrowRight} alt="Arrow right" />
+      <img src={ArrowRightUp} alt="Arrow right up" />
+      <img src={ArrowRightDown} alt="Arrow right down" />
+      <img src={ArrowLeftDown} alt="Arrow left down" />
+      <img src={ArrowLeftUp} alt="Arrow left up" />
+      <img src={ChevronUp} alt="Chevron up" />
+      <img src={ChevronDown} alt="Chevron down" />
+      <img src={ChevronLeft} alt="Chevron left" />
+      <img src={ChevronRight} alt="Chevron right" />
+      <img src={ChevronDoubleUp} alt="Chevron double up" />
+      <img src={ChevronDoubleDown} alt="Chevron double down" />
+      <img src={ChevronDoubleLeft} alt="Chevron double left" />
+      <img src={ChevronDoubleRight} alt="Chevron double right" />
+      <img src={CaretDown} alt="Caret down" />
+      <img src={CaretUp} alt="Caret up" />
+      <img src={CaretLeft} alt="Caret left" />
+      <img src={CaretRight} alt="Caret right" />
     </RowContainer>
     Functional
     <RowContainer>
-      <img src={Add} alt='Add' />
-      <img src={Archive} alt='Archive' />
-      <img src={Attachment} alt='Attachment' />
-      <img src={Backspace} alt='Backspace' />
-      <img src={Ban} alt='Ban' />
-      <img src={Bookmark} alt='Bookmark' />
-      <img src={Check} alt='Check' />
-      <img src={Close} alt='Close' />
-      <img src={CloudDownload} alt='Cloud download' />
-      <img src={CloudUpload} alt='Cloud upload' />
-      <img src={Copy} alt='Copy' />
-      <img src={DeleteAlt} alt='Delete alt' />
-      <img src={Delete} alt='Delete' />
-      <img src={Download} alt='Download' />
-      <img src={EditAlt} alt='Edit alt' />
-      <img src={Edit} alt='Edit' />
-      <img src={Enter} alt='Enter' />
-      <img src={Expand} alt='Expand' />
-      <img src={Export} alt='Export' />
-      <img src={ExternalLink} alt='External link' />
-      <img src={EyeOff} alt='Eye off' />
-      <img src={Eye} alt='Eye' />
-      <img src={Favorite} alt='Favorite' />
-      <img src={FilterAlt} alt='Filter alt' />
-      <img src={Filter} alt='Filter' />
-      <img src={Flag} alt='Flag' />
-      <img src={Heart} alt='Heart' />
-      <img src={LinkAlt} alt='Link alt' />
-      <img src={Link} alt='Link' />
-      <img src={Lock} alt='Lock' />
-      <img src={Login} alt='Login' />
-      <img src={Logout} alt='Logout' />
-      <img src={Menu} alt='Menu' />
-      <img src={Minimize} alt='Minimize' />
-      <img src={OptionsHorizontal} alt='Options horizontal' />
-      <img src={OptionsVertical} alt='Options vertical' />
-      <img src={Percentage} alt='Percentage' />
-      <img src={Refresh} alt='Refresh' />
-      <img src={Remove} alt='Remove' />
-      <img src={ReorderAlt} alt='Reorder alt' />
-      <img src={Reorder} alt='Reorder' />
-      <img src={Save} alt='Save' />
-      <img src={Search} alt='Search' />
-      <img src={Select} alt='Select' />
-      <img src={Send} alt='Sen' />
-      <img src={Settings} alt='Settings' />
-      <img src={Share} alt='Share' />
-      <img src={Sort} alt='Sort' />
-      <img src={Switch} alt='Switch' />
-      <img src={Undo} alt='Undo' />
-      <img src={Unlock} alt='Unlock' />
-      <img src={ZoomIn} alt='Zoom in' />
-      <img src={ZoomOut} alt='Zoom out' />
+      <img src={Add} alt="Add" />
+      <img src={Archive} alt="Archive" />
+      <img src={Attachment} alt="Attachment" />
+      <img src={Backspace} alt="Backspace" />
+      <img src={Ban} alt="Ban" />
+      <img src={Bookmark} alt="Bookmark" />
+      <img src={Check} alt="Check" />
+      <img src={Close} alt="Close" />
+      <img src={CloudDownload} alt="Cloud download" />
+      <img src={CloudUpload} alt="Cloud upload" />
+      <img src={Copy} alt="Copy" />
+      <img src={DeleteAlt} alt="Delete alt" />
+      <img src={Delete} alt="Delete" />
+      <img src={Download} alt="Download" />
+      <img src={EditAlt} alt="Edit alt" />
+      <img src={Edit} alt="Edit" />
+      <img src={Enter} alt="Enter" />
+      <img src={Expand} alt="Expand" />
+      <img src={Export} alt="Export" />
+      <img src={ExternalLink} alt="External link" />
+      <img src={EyeOff} alt="Eye off" />
+      <img src={Eye} alt="Eye" />
+      <img src={Favorite} alt="Favorite" />
+      <img src={FilterAlt} alt="Filter alt" />
+      <img src={Filter} alt="Filter" />
+      <img src={Flag} alt="Flag" />
+      <img src={Heart} alt="Heart" />
+      <img src={LinkAlt} alt="Link alt" />
+      <img src={Link} alt="Link" />
+      <img src={Lock} alt="Lock" />
+      <img src={Login} alt="Login" />
+      <img src={Logout} alt="Logout" />
+      <img src={Menu} alt="Menu" />
+      <img src={Minimize} alt="Minimize" />
+      <img src={OptionsHorizontal} alt="Options horizontal" />
+      <img src={OptionsVertical} alt="Options vertical" />
+      <img src={Percentage} alt="Percentage" />
+      <img src={Refresh} alt="Refresh" />
+      <img src={Remove} alt="Remove" />
+      <img src={ReorderAlt} alt="Reorder alt" />
+      <img src={Reorder} alt="Reorder" />
+      <img src={Save} alt="Save" />
+      <img src={Search} alt="Search" />
+      <img src={Select} alt="Select" />
+      <img src={Send} alt="Sen" />
+      <img src={Settings} alt="Settings" />
+      <img src={Share} alt="Share" />
+      <img src={Sort} alt="Sort" />
+      <img src={Switch} alt="Switch" />
+      <img src={Undo} alt="Undo" />
+      <img src={Unlock} alt="Unlock" />
+      <img src={ZoomIn} alt="Zoom in" />
+      <img src={ZoomOut} alt="Zoom out" />
     </RowContainer>
     Informational
     <RowContainer>
-      <img src={CircleAdd} alt='CircleAdd' />
-      <img src={CircleAddDown} alt='CircleAddDown' />
-      <img src={CircleAddLeft} alt='CircleAddLeft' />
-      <img src={CircleAddRight} alt='CircleAddRight' />
-      <img src={CircleAddUp} alt='CircleAddUp' />
-      <img src={CircleCheck} alt='CircleCheck' />
-      <img src={CircleError} alt='CircleError' />
-      <img src={CircleHelp} alt='CircleHelp' />
-      <img src={CircleInformation} alt='CircleInformation' />
-      <img src={CircleRemove} alt='CircleRemove' />
-      <img src={CircleWarning} alt='CircleWarning' />
-      <img src={Circle} alt='Circle' />
-      <img src={Warning} alt='Warning' />
+      <img src={CircleAdd} alt="CircleAdd" />
+      <img src={CircleAddDown} alt="CircleAddDown" />
+      <img src={CircleAddLeft} alt="CircleAddLeft" />
+      <img src={CircleAddRight} alt="CircleAddRight" />
+      <img src={CircleAddUp} alt="CircleAddUp" />
+      <img src={CircleCheck} alt="CircleCheck" />
+      <img src={CircleError} alt="CircleError" />
+      <img src={CircleHelp} alt="CircleHelp" />
+      <img src={CircleInformation} alt="CircleInformation" />
+      <img src={CircleRemove} alt="CircleRemove" />
+      <img src={CircleWarning} alt="CircleWarning" />
+      <img src={Circle} alt="Circle" />
+      <img src={Warning} alt="Warning" />
     </RowContainer>
     Objects
     <RowContainer>
-      <img src={BarChartAlt} alt='BarChartAlt' />
-      <img src={BarChart} alt='BarChart' />
-      <img src={Book} alt='Book' />
-      <img src={Calendar} alt='Calendar' />
-      <img src={ClipboardCheck} alt='ClipboardCheck' />
-      <img src={ClipboardList} alt='ClipboardList' />
-      <img src={Clipboard} alt='Clipboard' />
-      <img src={Clock} alt='Clock' />
-      <img src={Comment} alt='Comment' />
-      <img src={Compass} alt='Compass' />
-      <img src={CreditCard} alt='CreditCard' />
-      <img src={Database} alt='Database' />
-      <img src={DocumentAdd} alt='DocumentAdd' />
-      <img src={DocumentCheck} alt='DocumentCheck' />
-      <img src={DocumentDownload} alt='DocumentDownload' />
-      <img src={DocumentEmpty} alt='DocumentEmpty' />
-      <img src={DocumentRemove} alt='DocumentRemove' />
-      <img src={Document} alt='Document' />
-      <img src={FolderAdd} alt='FolderAdd' />
-      <img src={FolderCheck} alt='FolderCheck' />
-      <img src={FolderDownload} alt='FolderDownload' />
-      <img src={FolderRemove} alt='FolderRemove' />
-      <img src={Folder} alt='Folder' />
-      <img src={Home} alt='Home' />
-      <img src={Image} alt='Image' />
-      <img src={Inbox} alt='Inbox' />
-      <img src={Location} alt='Location' />
-      <img src={Map} alt='Map' />
-      <img src={Megaphone} alt='Megaphone' />
-      <img src={NotificationOff} alt='NotificationOff' />
-      <img src={Notification} alt='Notification' />
-      <img src={Pen} alt='Pen' />
-      <img src={Pin} alt='Pin' />
-      <img src={Print} alt='Print' />
-      <img src={ShoppingCartAlt} alt='ShoppingCartAlt' />
-      <img src={ShoppingCart} alt='ShoppingCart' />
-      <img src={Tag} alt='Tag' />
-      <img src={Webcam} alt='Webcam' />
+      <img src={BarChartAlt} alt="BarChartAlt" />
+      <img src={BarChart} alt="BarChart" />
+      <img src={Book} alt="Book" />
+      <img src={Calendar} alt="Calendar" />
+      <img src={ClipboardCheck} alt="ClipboardCheck" />
+      <img src={ClipboardList} alt="ClipboardList" />
+      <img src={Clipboard} alt="Clipboard" />
+      <img src={Clock} alt="Clock" />
+      <img src={Comment} alt="Comment" />
+      <img src={Compass} alt="Compass" />
+      <img src={CreditCard} alt="CreditCard" />
+      <img src={Database} alt="Database" />
+      <img src={DocumentAdd} alt="DocumentAdd" />
+      <img src={DocumentCheck} alt="DocumentCheck" />
+      <img src={DocumentDownload} alt="DocumentDownload" />
+      <img src={DocumentEmpty} alt="DocumentEmpty" />
+      <img src={DocumentRemove} alt="DocumentRemove" />
+      <img src={Document} alt="Document" />
+      <img src={FolderAdd} alt="FolderAdd" />
+      <img src={FolderCheck} alt="FolderCheck" />
+      <img src={FolderDownload} alt="FolderDownload" />
+      <img src={FolderRemove} alt="FolderRemove" />
+      <img src={Folder} alt="Folder" />
+      <img src={Home} alt="Home" />
+      <img src={Image} alt="Drawing" />
+      <img src={Inbox} alt="Inbox" />
+      <img src={Location} alt="Location" />
+      <img src={Map} alt="Map" />
+      <img src={Megaphone} alt="Megaphone" />
+      <img src={NotificationOff} alt="NotificationOff" />
+      <img src={Notification} alt="Notification" />
+      <img src={Pen} alt="Pen" />
+      <img src={Pin} alt="Pin" />
+      <img src={Print} alt="Print" />
+      <img src={ShoppingCartAlt} alt="ShoppingCartAlt" />
+      <img src={ShoppingCart} alt="ShoppingCart" />
+      <img src={Tag} alt="Tag" />
+      <img src={Webcam} alt="Webcam" />
     </RowContainer>
     Communication
     <RowContainer>
-      <img src={Call} alt='Call' />
-      <img src={Email} alt='Email' />
-      <img src={MessageAlt} alt='MessageAlt' />
-      <img src={Message} alt='Message ' />
+      <img src={Call} alt="Call" />
+      <img src={Email} alt="Email" />
+      <img src={MessageAlt} alt="MessageAlt" />
+      <img src={Message} alt="Message " />
     </RowContainer>
     Media
     <RowContainer>
-      <img src={Next} alt='Next' />
-      <img src={Pause} alt='Pause' />
-      <img src={Play} alt='Play' />
-      <img src={Previous} alt='Previous' />
-      <img src={Repeat} alt='Repeat' />
-      <img src={Shuffle} alt='Shuffle' />
-      <img src={Stop} alt='Stop' />
-      <img src={VolumeOff} alt='VolumeOff' />
-      <img src={VolumeUp} alt='VolumeUp' />
+      <img src={Next} alt="Next" />
+      <img src={Pause} alt="Pause" />
+      <img src={Play} alt="Play" />
+      <img src={Previous} alt="Previous" />
+      <img src={Repeat} alt="Repeat" />
+      <img src={Shuffle} alt="Shuffle" />
+      <img src={Stop} alt="Stop" />
+      <img src={VolumeOff} alt="VolumeOff" />
+      <img src={VolumeUp} alt="VolumeUp" />
     </RowContainer>
     Devices
     <RowContainer>
-      <img src={Camera} alt='Camera' />
-      <img src={Computer} alt='Computer' />
-      <img src={Laptop} alt='Laptop' />
-      <img src={Mobile} alt='Mobile' />
-      <img src={Speakers} alt='Speakers' />
-      <img src={Tablet} alt='Tablet' />
+      <img src={Camera} alt="Camera" />
+      <img src={Computer} alt="Computer" />
+      <img src={Laptop} alt="Laptop" />
+      <img src={Mobile} alt="Mobile" />
+      <img src={Speakers} alt="Speakers" />
+      <img src={Tablet} alt="Tablet" />
     </RowContainer>
     Views
     <RowContainer>
-      <img src={Board} alt='Board' />
-      <img src={Grid} alt='Grid' />
-      <img src={Layers} alt='Layers' />
-      <img src={Layout} alt='Layout' />
-      <img src={Table} alt='Table' />
-      <img src={ThreeRows} alt='ThreeRows' />
-      <img src={TwoColumns} alt='TwoColumns' />
-      <img src={TwoRows} alt='TwoRows' />
-      <img src={Window} alt='Window' />
+      <img src={Board} alt="Board" />
+      <img src={Grid} alt="Grid" />
+      <img src={Layers} alt="Layers" />
+      <img src={Layout} alt="Layout" />
+      <img src={Table} alt="Table" />
+      <img src={ThreeRows} alt="ThreeRows" />
+      <img src={TwoColumns} alt="TwoColumns" />
+      <img src={TwoRows} alt="TwoRows" />
+      <img src={Window} alt="Window" />
     </RowContainer>
     Weather
     <RowContainer>
-      <img src={Cloud} alt='Cloud' />
-      <img src={Cloudy} alt='Cloudy' />
-      <img src={Drop} alt='Drop' />
-      <img src={Fog} alt='Fog' />
-      <img src={Moon} alt='Moon' />
-      <img src={Rain} alt='Rain' />
-      <img src={Snow} alt='Snow' />
-      <img src={Snowflake} alt='Snowflake' />
-      <img src={Storm} alt='Storm' />
-      <img src={Sun} alt='Sun' />
-      <img src={Sunrise} alt='Sunrise' />
-      <img src={SunriseAlt} alt='SunriseAlt' />
-      <img src={Sunset} alt='Sunset' />
-      <img src={Temperature} alt='Temperature' />
-      <img src={Wind} alt='Wind' />
+      <img src={Cloud} alt="Cloud" />
+      <img src={Cloudy} alt="Cloudy" />
+      <img src={Drop} alt="Drop" />
+      <img src={Fog} alt="Fog" />
+      <img src={Moon} alt="Moon" />
+      <img src={Rain} alt="Rain" />
+      <img src={Snow} alt="Snow" />
+      <img src={Snowflake} alt="Snowflake" />
+      <img src={Storm} alt="Storm" />
+      <img src={Sun} alt="Sun" />
+      <img src={Sunrise} alt="Sunrise" />
+      <img src={SunriseAlt} alt="SunriseAlt" />
+      <img src={Sunset} alt="Sunset" />
+      <img src={Temperature} alt="Temperature" />
+      <img src={Wind} alt="Wind" />
     </RowContainer>
     WYSIWYG
     <RowContainer>
-      <img src={Bold} alt='Bold' />
-      <img src={Italic} alt='Italic' />
-      <img src={List} alt='List' />
-      <img src={Strikethrough} alt='Strikethrough' />
-      <img src={Text} alt='Text' />
-      <img src={Underline} alt='Underline' />
+      <img src={Bold} alt="Bold" />
+      <img src={Italic} alt="Italic" />
+      <img src={List} alt="List" />
+      <img src={Strikethrough} alt="Strikethrough" />
+      <img src={Text} alt="Text" />
+      <img src={Underline} alt="Underline" />
     </RowContainer>
     User
     <RowContainer>
-      <img src={User} alt='User' />
-      <img src={UserAdd} alt='UserAdd' />
-      <img src={UserRemove} alt='UserRemove' />
-      <img src={UserCheck} alt='UserCheck' />
-      <img src={Users} alt='Users' />
+      <img src={User} alt="User" />
+      <img src={UserAdd} alt="UserAdd" />
+      <img src={UserRemove} alt="UserRemove" />
+      <img src={UserCheck} alt="UserCheck" />
+      <img src={Users} alt="Users" />
     </RowContainer>
   </IconsContainer>
 );
